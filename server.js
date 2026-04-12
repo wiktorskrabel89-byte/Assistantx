01e43a9df8433d195d38a2e600bc12cb572163e8
unknown <wiktorskrabel89@gmail.com>
2026-04-12 20:30:18 +0200

    fix: always bind to 0.0.0.0 unconditionally

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const next = require("next");
 const { WebSocketServer, WebSocket } = require("ws");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = dev ? "localhost" : "0.0.0.0";
+const hostname = "0.0.0.0";
 const port = parseInt(process.env.PORT || "3000", 10);
 
 const app = next({ dev, hostname, port });
