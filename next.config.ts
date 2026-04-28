45c0c7b094e5de52c133795aafa7bc149429b536
wiktorskrabel89-byte <wiktorskrabel89@gmail.com>
2026-04-28 21:32:49 +0200

    feat: add Content-Security-Policy header to allow Next.js scripts and improve CSP

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ const nextConfig = {
 	generateEtags: false,
 	headers: async () => [
 		{
+			source: '/(.*)',
+			headers: [
+				{
+					key: 'Content-Security-Policy',
+					value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://assistantx.pl; style-src 'self' 'unsafe-inline'; img-src * blob: data:; font-src 'self'; connect-src *; frame-src 'self';"
+				},
+			],
+		},
+		{
 			source: '/_next/static/:path*',
 			headers: [
 				{
